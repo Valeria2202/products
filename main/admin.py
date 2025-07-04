5f8b312d4529809ceacba96d1fd7a4c4201dedd1
Valeria <vn96329@gmail.com>
2025-07-04 12:20:47 +0300

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product, Review, Client
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    fields = ['text', 'mark']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
+    list_display = ['id', 'title', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
---
+++ main/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+VERY_BAD = 1
+BAD = 2
+SATISFACTORY = 3
+GOOD = 4
+PERFECT = 5
+
+MARK_CHOICES = (
+    (VERY_BAD, "Очень плохо"),
+    (BAD, "Плохо"),
+    (SATISFACTORY, "Удовлетворительно"),
+    (GOOD, "Хорошо"),
+    (PERFECT, "Отлично")
+)
+
+class Client(models.Model):
+    pass
+
+class Product(models.Model):
+    objects = models.Manager()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    objects = models.Manager()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    text = models.TextField()
+    mark = models.PositiveSmallIntegerField(choices=MARK_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
